f4fdca5097c1d6fb2b45d09c114ea39cc8f5cdac
mathieulinel <mathieu.linel@protonmail.com>
2026-05-28 15:42:14 +0200

    add function to set path depending on environment variable

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+# main.py
 from fastapi import FastAPI
 from database import create_db_and_tables, engine
-from services import load_data, set_db
+from services import load_data
 from models.users import User
 from models.products import Product
 from routers.users import router as users_router
 from routers.products import router as products_router
+import os
+import shutil
 
+DATA_DIR = os.getenv("DATA_DIR", "data")
+os.makedirs(DATA_DIR, exist_ok=True)
+
+def set_path(item: str) -> str:
+    dest = os.path.join(DATA_DIR, f"{item}.json")
+    if not os.path.exists(dest):
+        shutil.copy(os.path.join("data", f"{item}.json"), dest)
+    return dest
 
 app = FastAPI(title="this is the api")
 
@@ -14,7 +25,6 @@ def on_startup():
     create_db_and_tables()
     load_data(set_path("users"), User, engine)
     load_data(set_path("products"), Product, engine)
-
 
 app.include_router(users_router)
 app.include_router(products_router)
