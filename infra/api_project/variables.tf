e60c052957cdc1a3655ff0c075ebdb85e8cce8cc
mathieulinel <mathieu.linel@protonmail.com>
2026-05-29 13:58:30 +0200

    define variables blueprint

---
+++ infra/api_project/variables.tf
@@ -0,0 +1,39 @@
+variable "suffix" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "cloudrun_id" {
+  type = string
+}
+
+variable "db" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_host" {
+  type = string
+}
+
+variable "db_port" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "data_dir" {
+  type = string
+}
+
+variable "bucket_id" {
+  type = string
+}
