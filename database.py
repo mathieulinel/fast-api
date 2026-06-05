4634432b3677e7f55700cc1e96874d64d75ed593
mathieulinel <mathieu.linel@protonmail.com>
2026-06-05 15:20:43 +0200

    Remove sqlite specifics configs. Add password env var

--- database.py
+++ database.py
@@ -9,10 +9,12 @@ db_name = os.getenv('DB_NAME')
 db_port = os.getenv('DB_PORT')
 db_host = os.getenv('DB_HOST')
 db_username = os.getenv('DB_USERNAME')
+db_password = os.getenv('DB_PASSWORD')
 
-db_url = set_db(db, port=db_port, host=db_host, database=db_name, username=db_username)
-connect_args = {"check_same_thread": False}
-engine = create_engine(db_url, echo=True, connect_args=connect_args)
+db_url = set_db(db, port=db_port, host=db_host, database=db_name, username=db_username, password=db_password)
+## sqlite config specific
+# connect_args = {"check_same_thread": False}
+engine = create_engine(db_url, echo=True) #, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.drop_all(engine)
